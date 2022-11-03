5f575b09d86767db4a23f192750bf49e1c51b79c
PokeyBoa <pokeyboa.mystic@gmail.com>
2022-11-03 21:51:13 +0800

    Perf: 优化了gunicorn配置项

--- modules/flask/flaskProdDeploy/gunicorn.config.py
+++ modules/flask/flaskProdDeploy/gunicorn.config.py
@@ -1,24 +1,33 @@
+from gevent import monkey
+monkey.patch_all()
+import multiprocessing
+
 ## 详细参考 https://docs.gunicorn.org/en/latest/configure.html
 
+# debug = True               # 开发者调试模式，生产关闭
+
 # 日志级别
-glevel = "debug"
-
-# 设置访问日志和错误信息日志路径
-accesslog = './logs/access.log'
-errorlog = './logs/error.log'
-
-# 并行工作进程数
-workers = 4
-
-# 设置超时时间（默认30s）
-timeout=60
-
-# 设置守护进程
-daemon = True
-
-# 设置工作模式为协程（依赖三方模块）
-worker_class = "gevent"
+loglevel = 'debug'
 
 # 绑定ip和监听端口
-bind = "0.0.0.0:8000"
+bind = '127.0.0.1:8000'      # 绑定与Nginx通信的端口
+
+# 设置pid文件
+pidfile = 'logs/gunicorn.pid'
+
+# 设置日志路径
+accesslog = 'logs/access.log'
+errorlog = 'logs/error.log'
+
+# 设置守护进程
+# daemon = True              # 如果用Supervisor来管理的话，需要注释该选项
+
+# 并行工作进程数
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# 设置工作模式为协程
+worker_class = 'gevent'      # 默认为阻塞模式，最好选择Gevent模式（三方模块）
+
+# 设置超时时间
+timeout = 60                 # 默认30s
 
